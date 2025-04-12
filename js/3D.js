2dd8e239c9f2d1ce447fd324b6155802b814b1b3
royeryannis <164513760+royeryannis@users.noreply.github.com>
2025-04-12 14:35:47 +0200

    Update 3D.js
    test js 3D

--- js/3D.js
+++ js/3D.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.152.2/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.152.2/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 function initializeViewer(modelPath, idCanvas) {
     // Récupérer le canvas existant
     const canvas = document.getElementById(idCanvas);
